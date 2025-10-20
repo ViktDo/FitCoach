c2c58a806d5e1854ead328dd4fba6a92c90fa0bc
ViktDo <viktdo@gmail.com>
2025-10-20 17:05:00 +0000

    Обновил базовые страницы

--- apps/fitcoach-next/next.config.js
+++ apps/fitcoach-next/next.config.js
@@ -1,9 +1,15 @@
+// apps/fitcoach-next/next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 's3.beget.com' }
-    ]
-  }
+    remotePatterns: [{ protocol: 'https', hostname: 's3.beget.com' }],
+  },
+  webpack: (config) => {
+    // Алиас, чтобы '@/...' всегда резолвился в папку 'app'
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    return config;
+  },
 };
